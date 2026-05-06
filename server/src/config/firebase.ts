7a4bf62c823b777582a4089b84d98d7111472a59
Ej Violata <ejayviolata982@gmail.com>
2026-05-06 16:07:12 +0800

    debug firebase env vars

--- server/src/config/firebase.ts
+++ server/src/config/firebase.ts
@@ -2,6 +2,10 @@ import admin from 'firebase-admin';
 import dotenv from 'dotenv';
 dotenv.config();
 
+console.log('PROJECT_ID:', process.env.FIREBASE_PROJECT_ID);
+console.log('CLIENT_EMAIL:', process.env.FIREBASE_CLIENT_EMAIL);
+console.log('PRIVATE_KEY exists:', !!process.env.FIREBASE_PRIVATE_KEY);
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
