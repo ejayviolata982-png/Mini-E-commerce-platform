5b2975ddf4fcd43dde172b1ecd91081bcd85121e
Ej Violata <ejayviolata982@gmail.com>
2026-05-06 16:29:24 +0800

    fix fileReplacements for production build

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://shopease-server-lqqa.onrender.com/api',
-  firebase: {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-  }
+  apiUrl: 'https://shopease-server-lqqa.onrender.com/api'
 };
